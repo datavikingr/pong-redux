345615db12cb599980c1e4871486070f0ce79897
dataviking <alexanderwantshislibraryback@gmail.com>
2023-04-29 00:51:45 -0400

    some data borked. Recovering. Still made some progress.

--- scripts/Player Layer.gd
+++ scripts/Player Layer.gd
@@ -1,13 +1,42 @@
 extends Node2D
 
-var court_size = Vector2(240, 135)  # the size of the court
+var ball
+var court_size = Vector2(240, 135) # the size of the court
 
-func is_out_of_bounds() -> bool:
-	var current_position = get_position()
-	return current_position.x > court_size.x or current_position.y > court_size.y or current_position.x < 0 or current_position.y < 0
+func _ready():
+	ball = get_node("Ball")
 
 func _physics_process(_delta):
 	if is_out_of_bounds():
-		var board_node = get_node("Player Layer")
-		board_node.spawn_ball()  # add the new ball to the scene tree
-		queue_free() # deletes this ball from the scene
+		spawn_ball()  # add the new ball to the scene tree
+		ball.queue_free() # deletes this ball from the scene
+
+func spawn_ball():
+	var ball_scene = load("res://tscn/ball.tscn")
+	var new_ball = ball_scene.instantiate()
+	new_ball.name = "Ball"  # Set the name of the new node to "Ball"
+	new_ball.position = Vector2(court_size.x/2, court_size.y/2)
+	add_child(new_ball)
+	ball = get_node("Ball")  # Ensure that the new node can be accessed with % notation
+
+func is_out_of_bounds() -> bool:
+	while ball == null:
+		spawn_ball()
+		ball = get_node("Ball")
+	var current_position = ball.get_position()
+	return current_position.x > court_size.x or current_position.y > court_size.y or current_position.x < 0 or current_position.y < 0
+
+func _on_ball_tree_exited():
+	ball = null
+
+func _on_goal_line_west_body_entered(body):
+	#Check for player or ball collision
+	if body.is_in_group("ball"):
+		print("GOAL BY Right Player")
+	pass # Replace with function body.
+
+
+func _on_goal_line_east_body_entered(body):
+	if body.is_in_group("ball"):
+		print("GOAL BY Left Player")
+	pass # Replace with function body.
--- scripts/Player Left.gd
+++ scripts/Player Left.gd
@@ -1,16 +1,22 @@
 extends RigidBody2D
 
+var ball
 @export var speed = 150
 
 func _physics_process(_delta):
-    var input_direction = Vector2.ZERO
-    input_direction = Vector2(
-        Input.get_action_strength("playerleft_right") - Input.get_action_strength("playerleft_left"),
-        Input.get_action_strength("playerleft_down") - Input.get_action_strength("playerleft_up")
-    )
-
-    linear_velocity = input_direction * speed
-    rotation = 0
+	var input_direction = Vector2.ZERO
+	input_direction = Vector2(
+		Input.get_action_strength("playerleft_right") - Input.get_action_strength("playerleft_left"),
+		Input.get_action_strength("playerleft_down") - Input.get_action_strength("playerleft_up")
+	)
+	linear_velocity = input_direction * speed
+	rotation = 0
+	var killswitch = Input.get_action_strength("kill")
+	if killswitch > 0:
+		print("KILL")
+		ball = get_node_or_null("../Ball")
+		if ball != null:
+			ball.queue_free()
 
 func _on_body_entered(_body):
-    angular_velocity = 0
+	angular_velocity = 0
