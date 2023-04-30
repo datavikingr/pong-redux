c11d4d9cc0bb0f6b5f05f8c5076ce653dcc1f679
futurehaskins <alexanderwantshislibraryback@gmail.com>
2023-04-30 16:56:05 -0400

    score display works as intended.

--- scripts/UI Elements.gd
+++ scripts/UI Elements.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var leftscore = 0
 @export var rightscore = 0
+@export var leftgames = 0
+@export var rightgames = 0
 signal goal_splash_begin
 var ball_name
 var highlight_ball
@@ -11,25 +13,46 @@ func _ready() -> void:
 	pass
 
 func _on_player_layer_goal(player):
+	ball_name = null
+	scoring_player = null
+	highlight_ball = null
 	if player == "Left":
-		print("left signal received")
-		leftscore += 1
-		print(leftscore)
 		goal_splash_begin.emit()
-		if leftscore >= 10:
-			print("Left Wins")
+		leftscore += 1
 		ball_name = "ScoreBall"+str(leftscore)
 		scoring_player = get_node("Player Left UI")
 		highlight_ball = scoring_player.get_node(ball_name)
 		highlight_ball.modulate.a = 1
+		if leftscore >= 10: # This is round win condition
+			leftscore = 0
+			for i in range(1,11):
+				ball_name = "ScoreBall"+str(i)
+				highlight_ball = scoring_player.get_node(ball_name)
+				highlight_ball.modulate.a = 0.25
+			leftgames += 1
+			ball_name = "RoundBall"+str(leftgames)
+			highlight_ball = scoring_player.get_node(ball_name)
+			highlight_ball.modulate.a = 1
+			if leftgames >=2:
+				print("Left Player wins the match")
+				#TODO: Match win code
 	else:
-		print("right signal received")
-		rightscore += 1
-		print(rightscore)
 		goal_splash_begin.emit()
-		if rightscore >= 10:
-			print("Right Wins")
+		rightscore += 1
 		ball_name = "ScoreBall"+str(rightscore)
 		scoring_player = get_node("Player Right UI")
 		highlight_ball = scoring_player.get_node(ball_name)
 		highlight_ball.modulate.a = 1
+		if rightscore >= 10: # This is round win condition
+			rightscore = 0
+			for i in range(1,11):
+				ball_name = "ScoreBall"+str(i)
+				highlight_ball = scoring_player.get_node(ball_name)
+				highlight_ball.modulate.a = 0.25
+			rightgames += 1
+			ball_name = "RoundBall"+str(rightgames)
+			highlight_ball = scoring_player.get_node(ball_name)
+			highlight_ball.modulate.a = 1
+			if rightgames >=2:
+				print("Right Player wins the match")
+				#TODO: Match win code
