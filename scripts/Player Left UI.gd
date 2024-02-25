2d7726cf6f1d88800e63a264e788e94658433dc4
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2024-02-25 10:59:49 -0500

    organization and cleanup

--- scripts/Player Left UI.gd
+++ scripts/Player Left UI.gd
@@ -1,17 +1,10 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var ui_color = %"Player West".playercolor
-	# Iterate over children nodes named "ScoreballX" so we can change their color; Changing golden balls is sketchy
-	for ball in range(1, 11):
-		# Find the child
-		var ball_name = "ScoreBall"+str(ball)
-		# Collect the child
-		var scoreball = get_node(ball_name)
-		#Double check it's not a nothing node
-		if scoreball != null:
-			# Change that scoreball's color
-			scoreball.self_modulate = ui_color
+	for ball in range(1, 11): # Iterate over children nodes named "ScoreballX" so we can change their color; Changing golden balls is sketchy
+		var ball_name = "ScoreBall"+str(ball) # Find the child
+		var scoreball = get_node(ball_name) # Collect the child
+		if scoreball != null: #Double check it's not a nothing node
+			scoreball.self_modulate = ui_color # Change that scoreball's color
 
